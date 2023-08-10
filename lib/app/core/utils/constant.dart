ade99be91511c65899ef20b19ad948b3c396d25a
ghassen elkamel <92597241+ghassen-elkamel@users.noreply.github.com>
2023-08-10 21:38:37 +0100

    intial template

---
+++ lib/app/core/utils/constant.dart
@@ -0,0 +1,4 @@
+const String host = "localhost";
+const int port = 5000;
+const String apiPrefix = "/api";
+const Duration timeoutDuration = Duration(seconds: 30);
---
+++ lib/app/core/values/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+const Color primaryColor =Color(0xff004D7A);
+const Color secondaryColor =Color(0xff00A170);
+const Color accentColor =Color(0xffFFD700);
+const Color neutralColor =Color(0xffEFEFEF);
+const Color textColor =Color(0xff333333);
+const Color errorColor =Color(0xffFF5252);
+
+
+
+
+
